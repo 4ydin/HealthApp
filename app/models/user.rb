5dae65f63298a1fd9a451ab0f2912be55e0fc80e
4ydin <nidya.ayu24@yahoo.de>
2012-12-07 12:44:15 +0100

    signup_done
    removing all atributes that aren't needed by signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,9 @@ class User < ActiveRecord::Base
   attr_accessible :birth_date, :email, :password, :password_confirmation, :height, :name, :username, :weight
   has_secure_password
   acts_as_authentic
+
+  validates :email, presence:   true,
+                  uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 1}, :on => :create   
+  validates :password_confirmation, presence: { message: "Password can't be blank" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121207103903) do
+ActiveRecord::Schema.define(:version => 20121207113035) do
 
   create_table "foods", :force => true do |t|
     t.string   "name"
@@ -38,17 +38,11 @@ ActiveRecord::Schema.define(:version => 20121207103903) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
     t.string   "username"
-    t.date     "birth_date"
     t.string   "email"
     t.string   "password"
-    t.integer  "height"
-    t.integer  "weight"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
-    t.boolean  "admin",                 :default => false
-    t.datetime "last_login"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.string   "password_confirmation"
     t.string   "password_digest"
     t.string   "persistence_token"
