eadcb1fbd9aa76927578d3e0707c502ef462ebd3
nadnidnuds <nidya.ayu28@yahoo.com>
2013-01-18 18:20:16 +0100

    tips and ideal weight

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,8 +2,18 @@ class Profile < ActiveRecord::Base
   attr_accessible :height, :weight, :name, :birth_date, :gender
   belongs_to :user
   has_many :dailies,  dependent: :destroy
+  after_initialize :default_values
 
-  def countIdealWeight(g, h)
+
+    def default_values
+      self.name ||= "default data"
+      self.birth_date ||= "1990-11-11"
+      self.weight ||= 50
+      self.height ||= 160
+      self.gender ||= 'f'
+    end
+
+  def countIdealWeight(h, g)
     weight  = self.weight
   	if (g == 'f')
   		weight = h-110
