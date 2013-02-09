2453ca0dfcba1729f02363c1071522b487679aa1
4ydin <nidya.ayu24@yahoo.de>
2013-02-09 12:16:55 +0100

    db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209104920) do
+ActiveRecord::Schema.define(:version => 20130209111049) do
 
   create_table "dailies", :force => true do |t|
     t.integer  "total"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(:version => 20130209104920) do
     t.datetime "updated_at", :null => false
     t.string   "name"
     t.date     "birth_date"
-    t.string   "gender"
   end
 
   create_table "tips", :force => true do |t|
